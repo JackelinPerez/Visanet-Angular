d505db6b908ab9a6ef28b9571ced7e0bc1bf07c0
Heidy Gerbi <heidyjoanagr@gmail.com>
2019-05-08 21:30:43 -0500

    ts solicitud de prestamos - calculate

--- visanet/src/app/home/loan/calculate/calculate.component.ts
+++ visanet/src/app/home/loan/calculate/calculate.component.ts
@@ -6,10 +6,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./calculate.component.sass']
 })
 export class CalculateComponent implements OnInit {
-
+  objInfLoan = {};
   constructor() { }
 
   ngOnInit() {
+    // console.log(this.calculateLoan(10000, 12));  
+  }
+  calculateLoan(amount: number, months: number) {
+    return (amount * (100 + months))/months;
   }
 
+  paymentSummary(amount: number, months: number){
+    return {
+      loan: amount,
+      time: months,
+      totalPayment: amount * (100 + months)
+    }
+  }
+  reduceQuantity(amount:number){
+    if (amount >= 1000) return amount= amount - 1000;
+  }
+  addQuantity(amount:number){
+    return amount= amount + 1000;
+  }
+  sendLoanData(){
+    return this.objInfLoan;
+  }
 }
