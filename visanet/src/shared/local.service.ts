1c61a4f0287a0d570d8e724fee01c715ac8d93c3
JackelinPerez <filomenochahua@gmail.com>
2019-05-09 09:08:30 -0500

    checkbox router Loan

--- visanet/src/shared/local.service.ts
+++ visanet/src/shared/local.service.ts
@@ -9,11 +9,18 @@ export class LocalService {
   public calculateLoans = new BehaviorSubject({});
   calculateLoan = this.calculateLoans.asObservable();
 
+  private statusLoan = new BehaviorSubject({calculate: '1', data: '2', compare: '3', request: '4', set:'5'});
+  public statusLoanP = this.statusLoan.asObservable();
+
   objLoanData: {};
 
   constructor() { }
+  
   getLoanData(obj: Object){
     this.objLoanData= {...obj};
     this.calculateLoans.next(this.objLoanData);
   }
+  getStatusData(status: any){
+    this.statusLoan.next(status);
+  }  
 }
